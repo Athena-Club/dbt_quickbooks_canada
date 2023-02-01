6a053b73b7df313b075c4bf94743d1e4dff8fb8f
peterapati <peter.apati@hiflylabs.com>
2023-02-01 16:26:56 +0100

    added additional summaries

--- models/intermediate/int_quickbooks__additional_summaries.sql
+++ models/intermediate/int_quickbooks__additional_summaries.sql
@@ -27,13 +27,13 @@ gross_profit as (
 net_operating_income as (
     select
         cast('99992' as {{ dbt_utils.type_string() }}) as account_id,
-        cast('6999' as {{ dbt_utils.type_string() }}) as account_number,
-        cast('NET OPERATING INCOME' as {{ dbt_utils.type_string() }}) as account_name,
+        cast('6899' as {{ dbt_utils.type_string() }}) as account_number,
+        cast('NET OPERATING INCOME/EBIDTA' as {{ dbt_utils.type_string() }}) as account_name,
         false as is_sub_account,
         cast(null as {{ dbt_utils.type_string() }}) as parent_account_number,
         cast(null as {{ dbt_utils.type_string() }}) as parent_account_name,
-        cast('NET OPERATING INCOME' as {{ dbt_utils.type_string() }}) as account_type,
-        cast('NET OPERATING INCOME' as {{ dbt_utils.type_string() }})as account_sub_type,
+        cast('NET OPERATING INCOME/EBIDTA' as {{ dbt_utils.type_string() }}) as account_type,
+        cast('NET OPERATING INCOME/EBIDTA' as {{ dbt_utils.type_string() }})as account_sub_type,
         cast('Amount sum 3' as {{ dbt_utils.type_string() }}) as account_class,
         cast('income_statement' as {{ dbt_utils.type_string() }}) as financial_statement_helper,
         cast({{ dbt_utils.date_trunc("year", "period_first_day") }} as date) as date_year,
@@ -99,6 +99,111 @@ net_income as (
     group by period_first_day
 ),
 
+total_revenue as (
+    select
+        cast('99995' as {{ dbt_utils.type_string() }}) as account_id,
+        cast('4899' as {{ dbt_utils.type_string() }}) as account_number,
+        cast('TOTAL REVENUE' as {{ dbt_utils.type_string() }}) as account_name,
+        false as is_sub_account,
+        cast(null as {{ dbt_utils.type_string() }}) as parent_account_number,
+        cast(null as {{ dbt_utils.type_string() }}) as parent_account_name,
+        cast('TOTAL REVENUE' as {{ dbt_utils.type_string() }}) as account_type,
+        cast('TOTAL REVENUE' as {{ dbt_utils.type_string() }})as account_sub_type,
+        cast('Amount sum 5' as {{ dbt_utils.type_string() }}) as account_class,
+        cast('income_statement' as {{ dbt_utils.type_string() }}) as financial_statement_helper,
+        cast({{ dbt_utils.date_trunc("year", "period_first_day") }} as date) as date_year,
+        cast(period_first_day as date) as period_first_day,
+        {{ dbt_utils.last_day("period_first_day", "month") }} as period_last_day,
+        sum(period_net_change) as period_net_change
+    from general_ledger_balances
+    where account_type in ('Income')
+    group by period_first_day
+),
+
+total_cost_of_goods_sold as (
+    select
+        cast('99996' as {{ dbt_utils.type_string() }}) as account_id,
+        cast('5398' as {{ dbt_utils.type_string() }}) as account_number,
+        cast('TOTAL COST OF GOODS SOLD' as {{ dbt_utils.type_string() }}) as account_name,
+        false as is_sub_account,
+        cast(null as {{ dbt_utils.type_string() }}) as parent_account_number,
+        cast(null as {{ dbt_utils.type_string() }}) as parent_account_name,
+        cast('TOTAL COST OF GOODS SOLD' as {{ dbt_utils.type_string() }}) as account_type,
+        cast('TOTAL COST OF GOODS SOLD' as {{ dbt_utils.type_string() }})as account_sub_type,
+        cast('Amount sum 6' as {{ dbt_utils.type_string() }}) as account_class,
+        cast('income_statement' as {{ dbt_utils.type_string() }}) as financial_statement_helper,
+        cast({{ dbt_utils.date_trunc("year", "period_first_day") }} as date) as date_year,
+        cast(period_first_day as date) as period_first_day,
+        {{ dbt_utils.last_day("period_first_day", "month") }} as period_last_day,
+        sum(period_net_change*-1) as period_net_change
+    from general_ledger_balances
+    where account_type in ('Cost of Goods Sold')
+    group by period_first_day
+),
+
+total_expenses as (
+    select
+        cast('99997' as {{ dbt_utils.type_string() }}) as account_id,
+        cast('6898' as {{ dbt_utils.type_string() }}) as account_number,
+        cast('TOTAL EXPENSES' as {{ dbt_utils.type_string() }}) as account_name,
+        false as is_sub_account,
+        cast(null as {{ dbt_utils.type_string() }}) as parent_account_number,
+        cast(null as {{ dbt_utils.type_string() }}) as parent_account_name,
+        cast('TOTAL EXPENSES' as {{ dbt_utils.type_string() }}) as account_type,
+        cast('TOTAL EXPENSES' as {{ dbt_utils.type_string() }})as account_sub_type,
+        cast('Amount sum 7' as {{ dbt_utils.type_string() }}) as account_class,
+        cast('income_statement' as {{ dbt_utils.type_string() }}) as financial_statement_helper,
+        cast({{ dbt_utils.date_trunc("year", "period_first_day") }} as date) as date_year,
+        cast(period_first_day as date) as period_first_day,
+        {{ dbt_utils.last_day("period_first_day", "month") }} as period_last_day,
+        sum(period_net_change*-1) as period_net_change
+    from general_ledger_balances
+    where account_type in ('Expense')
+    group by period_first_day
+),
+
+total_other_income as (
+    select
+        cast('99998' as {{ dbt_utils.type_string() }}) as account_id,
+        cast('4999' as {{ dbt_utils.type_string() }}) as account_number,
+        cast('TOTAL OTHER INCOME' as {{ dbt_utils.type_string() }}) as account_name,
+        false as is_sub_account,
+        cast(null as {{ dbt_utils.type_string() }}) as parent_account_number,
+        cast(null as {{ dbt_utils.type_string() }}) as parent_account_name,
+        cast('TOTAL OTHER INCOME' as {{ dbt_utils.type_string() }}) as account_type,
+        cast('TOTAL OTHER IfullNCOME' as {{ dbt_utils.type_string() }})as account_sub_type,
+        cast('Amount sum 8' as {{ dbt_utils.type_string() }}) as account_class,
+        cast('income_statement' as {{ dbt_utils.type_string() }}) as financial_statement_helper,
+        cast({{ dbt_utils.date_trunc("year", "period_first_day") }} as date) as date_year,
+        cast(period_first_day as date) as period_first_day,
+        {{ dbt_utils.last_day("period_first_day", "month") }} as period_last_day,
+        sum(period_net_change) as period_net_change
+    from general_ledger_balances
+    where account_type in ('Other Income')
+    group by period_first_day
+),
+
+total_other_expenses as (
+    select
+        cast('99999' as {{ dbt_utils.type_string() }}) as account_id,
+        cast('9997' as {{ dbt_utils.type_string() }}) as account_number,
+        cast('TOTAL OTHER EXPENSES' as {{ dbt_utils.type_string() }}) as account_name,
+        false as is_sub_account,
+        cast(null as {{ dbt_utils.type_string() }}) as parent_account_number,
+        cast(null as {{ dbt_utils.type_string() }}) as parent_account_name,
+        cast('TOTAL OTHER EXPENSES' as {{ dbt_utils.type_string() }}) as account_type,
+        cast('TOTAL OTHER EXPENSES' as {{ dbt_utils.type_string() }})as account_sub_type,
+        cast('Amount sum 9' as {{ dbt_utils.type_string() }}) as account_class,
+        cast('income_statement' as {{ dbt_utils.type_string() }}) as financial_statement_helper,
+        cast({{ dbt_utils.date_trunc("year", "period_first_day") }} as date) as date_year,
+        cast(period_first_day as date) as period_first_day,
+        {{ dbt_utils.last_day("period_first_day", "month") }} as period_last_day,
+        sum(period_net_change*-1) as period_net_change
+    from general_ledger_balances
+    where account_type in ('Other Expense')
+    group by period_first_day
+),
+
 additional_summaries as (
     select * from gross_profit
     union
@@ -107,6 +212,16 @@ additional_summaries as (
     select * from net_other_income
     union
     select * from net_income    
+    union
+    select * from total_revenue
+    union
+    select * from total_cost_of_goods_sold
+    union
+    select * from total_expenses
+    union
+    select * from total_other_income
+    union
+    select * from total_other_expenses
 ),
 
 additional_summaries_ending as (
